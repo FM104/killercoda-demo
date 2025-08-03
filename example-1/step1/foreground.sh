fd7bd94930dd0559aa5be8f53f1dd23f3a4df07e
FM104 <103407003+FM104@users.noreply.github.com>
2025-08-03 21:34:37 +0800

    Update foreground.sh

--- example-1/step1/foreground.sh
+++ example-1/step1/foreground.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-touch /tmp/step1-forground
-date
+echo waiting for init-background-script to finish
+while [ ! -f /tmp/background0 ]; do sleep 1; done
+echo Hello and welcome to this scenario!
